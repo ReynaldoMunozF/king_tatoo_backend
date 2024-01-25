cc45438970f322bf04091c492f4d34b9bc6d847b
Reynaldo <reynaldo.munozf21@gmail.com>
2024-01-25 13:41:17 +0100

    change name to database

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   port: 3307,
   username: "root",
   password: "root",
-  database: "KING_TATOO",
+  database: "King_Tattoo",
   entities: [`${__dirname}/../models/**/*{.js,.ts}`],
   migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
   synchronize: false,
